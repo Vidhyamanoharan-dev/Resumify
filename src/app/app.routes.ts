4de07c20647c4e02f8e4c695883ed0f90f2dad26
sandra394 <sandrakchandran394@gmail.com>
2025-05-16 15:56:25 +0530

    updated home page and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,7 @@ import { LoginComponent } from './Auth/login/login.component';
 import { RegisterComponent } from './Auth/register/register.component';
 import { UploadresumeComponent } from './uploadresume/uploadresume.component';
 import { ParsedresumeComponent } from './parsedresume/parsedresume.component';
-import { ContactusComponent } from './home/contactus/contactus.component';
-import { AboutComponent } from './home/about/about.component';
+
 import { ForgotpassComponent } from './Auth/forgotpass/forgotpass.component';
 import { SelectedFilesComponent } from './selectedfiles/selectedfiles.component';
 import { ResumeEnhancingComponent } from './resume-enhancing/resume-enhancing.component';
@@ -19,16 +18,11 @@ export const routes: Routes = [
   { path: 'selectedfiles', component: SelectedFilesComponent },
   { path: 'enhance', component: ResumeEnhancingComponent },
 {path: 'parsedresumes', component: ParsedresumeComponent},
-  { path: 'contact', component: ContactusComponent },
-  { path: 'about', component: AboutComponent },
+
   { path: 'forgot-password', component: ForgotpassComponent },
 
   // Lazy loaded
-  {
-    path: 'features',
-    loadComponent: () =>
-      import('./home/features/features.component').then(m => m.FeaturesComponent),
-  },
+
 
   // Always last
   { path: '**', redirectTo: '' },
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,18 @@
-// home.component.ts
 import { Component } from '@angular/core';
-import { AboutComponent } from './about/about.component';
-import { ContactusComponent } from './contactus/contactus.component';
-import { FeaturesComponent } from './features/features.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { FooterComponent } from "./footer/footer.component";
+
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    AboutComponent,
-    ContactusComponent,
-    FeaturesComponent,
     NavbarComponent,
     MatIconModule,
     MatButtonModule,
     FooterComponent
-],
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
