ba81d6ab6f988765ae8c56a63bd91aa8c116e6d5
sandra394 <sandrakchandran394@gmail.com>
2025-06-03 10:20:11 +0530

    empty components

---
+++ src/app/footer-pages/termsofservice/termsofservice.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-termsofservice',
+  imports: [],
+  templateUrl: './termsofservice.component.html',
+  styleUrl: './termsofservice.component.scss'
+})
+export class TermsofserviceComponent {
+
+}
