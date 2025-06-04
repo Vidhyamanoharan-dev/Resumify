8826d62bcaf6fd21aa48a8f0cc6dae82f2409fec
Amaldevm <amaldevhari265@gmail.com>
2025-06-05 04:01:08 +0530

    created gdrp-ccpa \ pages in the footer

---
+++ src/app/home/footer/footer-pages/gdrp-ccpa/gdrp-ccpa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gdrp-ccpa',
+  imports: [],
+  templateUrl: './gdrp-ccpa.component.html',
+  styleUrl: './gdrp-ccpa.component.scss'
+})
+export class GDRPCCPAComponent {
+
+}
