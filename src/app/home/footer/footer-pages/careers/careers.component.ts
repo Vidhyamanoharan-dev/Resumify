c0ac3e045c23ff2c7ed231b1638645a1d09ce50b
Amaldevm <amaldevhari265@gmail.com>
2025-06-05 03:58:54 +0530

    created career pages in the footer

---
+++ src/app/home/footer/footer-pages/careers/careers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-careers',
+  imports: [],
+  standalone: true,
+  templateUrl: './careers.component.html',
+  styleUrl: './careers.component.scss'
+})
+export class CareersComponent {
+
+}
