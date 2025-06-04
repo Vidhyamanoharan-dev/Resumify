7d459eaa371ff314aa31c276567c9066b37ab128
Amaldevm <amaldevhari265@gmail.com>
2025-06-05 04:02:14 +0530

    created press pages in the footer

---
+++ src/app/home/footer/footer-pages/press/press.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-press',
+  imports: [],
+  standalone: true,
+  templateUrl: './press.component.html',
+  styleUrl: './press.component.scss'
+})
+export class PressComponent {
+
+}
