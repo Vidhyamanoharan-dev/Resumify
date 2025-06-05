c53e3754745d1b8200a7a782cf36dd40bd43b775
Amaldevm <amaldevhari265@gmail.com>
2025-06-05 16:57:34 +0530

    enhancements on termofservice components

--- src/app/home/footer/footer-pages/termsofservice/termsofservice.component.ts
+++ src/app/home/footer/footer-pages/termsofservice/termsofservice.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../footer.component";
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-termsofservice',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent,MatIconModule],
   templateUrl: './termsofservice.component.html',
   styleUrls: ['./termsofservice.component.scss']
 })
