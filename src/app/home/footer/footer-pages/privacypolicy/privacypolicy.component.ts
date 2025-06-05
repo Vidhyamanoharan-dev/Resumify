1b687980904d3b012fb2aa9566dd44caac765e42
Amaldevm <amaldevhari265@gmail.com>
2025-06-05 15:36:01 +0530

    enhancements on privacypolicy components

--- src/app/home/footer/footer-pages/privacypolicy/privacypolicy.component.ts
+++ src/app/home/footer/footer-pages/privacypolicy/privacypolicy.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../footer.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-privacypolicy',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent,MatIconModule],
   templateUrl: './privacypolicy.component.html',
   styleUrls: ['./privacypolicy.component.scss']
 })
